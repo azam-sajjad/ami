631c9aabca759e0dd964ea8a46f74716d2a6c58f
azamsajjad <azamsaws@gmail.com>
2024-04-17 18:31:45 +0500

    all_ami

--- scripts/deb/cleanup.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-# uninstall Ansible and Remove PPA
-sudo apt -y remove --purge ansible
-sudo apt-add-repository --remove ppa:ansible/ansible
-# Apt Cleanup
-sudo apt -y autoremove
-sudo apt -y update
--- scripts/ssm/ubuntu.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-sudo snap install amazon-ssm-agent --classic
-sudo snap start amazon-ssm-agent
