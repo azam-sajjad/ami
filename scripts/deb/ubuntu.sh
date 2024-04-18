1fd72ca757206cef1c5660c1a7d922e6920910bb
azamsajjad <azamsaws@gmail.com>
2024-04-18 17:16:56 +0500

    pkr

--- scripts/deb/ubuntu.sh
+++ scripts/deb/ubuntu.sh
@@ -4,7 +4,8 @@ sudo apt -y update && sudo apt -y upgrade
 sudo apt -y install software-properties-common
 sudo apt-add-repository ppa:ansible/ansible
 sudo apt -y update
-sudo apt -y install ansible
+sudo yum install python3-pip 
+pip3 install ansible
 # install ssm agent
 sudo snap install amazon-ssm-agent --classic
 sudo snap start amazon-ssm-agent
