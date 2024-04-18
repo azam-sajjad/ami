fd229fbc56f363d606ff460aec74bc1778e7ff51
azamsajjad <azamsaws@gmail.com>
2024-04-18 21:41:16 +0500

    ubuntu

--- scripts/deb/ubuntu.sh
+++ scripts/deb/ubuntu.sh
@@ -5,8 +5,8 @@ sudo apt -y install software-properties-common
 sudo apt-add-repository ppa:ansible/ansible
 sudo apt -y update
 sudo apt-get install ansible -y
-ansible-galaxy collection install ansible.posix
-ansible-galaxy collection install community.general
+sudo ansible-galaxy collection install ansible.posix
+sudo ansible-galaxy collection install community.general
 sudo mkdir -p /usr/share/ansible/collections
 sudo cp -r /root/.ansible/collections/ansible_collections /usr/share/ansible/collections/
 sudo chmod -R a+rx /usr/share/ansible/collections/
