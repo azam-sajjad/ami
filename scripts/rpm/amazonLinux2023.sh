89446a2b3840e3165dd091b947c260e6ddeca259
azamsajjad <azamsaws@gmail.com>
2024-04-19 22:55:28 +0500

    19f

--- scripts/rpm/amazonLinux2023.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-# install Ansible
-sudo yum clean all
-sudo yum install ansible -y
-ansible-galaxy collection install ansible.posix
-ansible-galaxy collection install community.general
-sudo mkdir -p /usr/share/ansible/collections
-sudo cp -r /root/.ansible/collections/ansible_collections /usr/share/ansible/collections/
-sudo chmod -R a+rx /usr/share/ansible/collections/
