ad41d9f8d462459560ba5fd926c11c4a082c9ea4
azamsajjad <azamsaws@gmail.com>
2024-04-19 17:15:35 +0500

    al2

--- scripts/rpm/amazonLinux2023.sh
+++ scripts/rpm/amazonLinux2023.sh
@@ -1,9 +1,6 @@
 #!/bin/bash
 # install Ansible
 sudo yum clean all
-sudo dnf config-manager --set-enabled crb
-sudo dnf install epel-release epel-next-release
-sudo yum-config-manager --enable epel
 sudo yum install ansible -y
 ansible-galaxy collection install ansible.posix
 ansible-galaxy collection install community.general
