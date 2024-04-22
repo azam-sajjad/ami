15e8cbcf2be055cfdbc29e431e6b1a4d9d5ee75a
azamsajjad <azamsaws@gmail.com>
2024-04-22 21:27:21 +0500

    deb

--- scripts/deb/debian.sh
+++ scripts/deb/debian.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 # install Ansible and PPA
 sudo apt -y update
-sudo apt -y install software-properties-common
-sudo apt-add-repository ppa:ansible/ansible
-sudo apt -y update
-sudo apt-get -y install ansible
-sudo apt install python3-venv python3-pip
+# sudo apt -y install software-properties-common
+# sudo apt-add-repository ppa:ansible/ansible
+# sudo apt -y update
+# sudo apt-get -y install ansible
+sudo apt-get install python3 python3-pip -y
+pip install ansible
 ansible --version
 ansible-galaxy collection install ansible.posix
 ansible-galaxy collection install community.general
