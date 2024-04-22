d2a641d016b79df2b7fe1a4815615576112a27a8
azamsajjad <azamsaws@gmail.com>
2024-04-22 21:22:50 +0500

    deb

--- scripts/deb/debian.sh
+++ scripts/deb/debian.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # install Ansible and PPA
-sudo apt -y update && sudo apt -y upgrade
+sudo apt -y update
 sudo apt -y install software-properties-common
 sudo apt-add-repository ppa:ansible/ansible
 sudo apt -y update
