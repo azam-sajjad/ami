b2a36646e40d93e5008fd89c96bfe8604ddb33ad
azamsajjad <azamsaws@gmail.com>
2024-04-23 14:49:27 +0500

    debian

--- scripts/deb/ubuntu.sh
+++ scripts/deb/ubuntu.sh
@@ -1,20 +1,27 @@
 #!/bin/bash
 # install Ansible and PPA
-sudo apt update && sudo apt -y upgrade
+# sudo apt update && sudo apt -y upgrade
+# sudo apt -y install software-properties-common
+# sudo apt-add-repository -y ppa:ansible/ansible
+# sudo add-apt-repository -y ppa:deadsnakes/ppa
+# sudo add-apt-repository -y universe
+# # sudo apt install python3.8 python3-virtualenv
+# # sudo echo "ubuntu ALL=(ALL) NOPASSWD:ALL" | sudo tee /etc/sudoers.d/ubuntu
+# sudo apt install ansible -y
+# # sudo apt install python3-pip
+# # pip3 install ansible
+# # virtualenv -p python3.8 venv-ansible
+# # source venv-ansible/bin/activate
+# # pip install ansible
+# # pip freeze
+#!/bin/bash
+# install Ansible and PPA
+sudo apt -y update && sudo apt -y upgrade
 sudo apt -y install software-properties-common
-sudo apt-add-repository -y ppa:ansible/ansible
-sudo add-apt-repository -y ppa:deadsnakes/ppa
-sudo add-apt-repository -y universe
+sudo apt-add-repository ppa:ansible/ansible
 sudo apt-get update
-# sudo apt install python3.8 python3-virtualenv
-# sudo echo "ubuntu ALL=(ALL) NOPASSWD:ALL" | sudo tee /etc/sudoers.d/ubuntu
-sudo apt install ansible -y
-# sudo apt install python3-pip
-# pip3 install ansible
-# virtualenv -p python3.8 venv-ansible
-# source venv-ansible/bin/activate
-# pip install ansible
-# pip freeze
+sudo apt -y update
+sudo apt -y install ansible
 ansible --version
 echo $PATH
 which ansible
