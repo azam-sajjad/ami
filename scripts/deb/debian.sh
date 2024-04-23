6b413d27d236e31de9a0e055fa88848b7b90a8e0
azamsajjad <azamsaws@gmail.com>
2024-04-23 22:17:18 +0500

    deb

--- scripts/deb/debian.sh
+++ scripts/deb/debian.sh
@@ -3,31 +3,15 @@
 echo "127.0.0.1 `hostname` localhost" >> /etc/hosts
 sudo apt -y update 
 sudo apt-get upgrade -y
-# sudo apt -y install software-properties-common
-# sudo apt-add-repository --yes --update ppa:ansible/ansible
-# sudo apt-get update --fix-missing -y
-# sudo apt -y install ansible
-# ansible --version
 python3
 sudo apt install python3-pip -y
 sudo apt install python3-virtualenv
 virtualenv -p python3 venv-ansible
 source venv-ansible/bin/activate
 pip list
-pip  install ansible
+pip install ansible
 ansible --version
 ansible-community --version
-pip install ansible
-python3 -m pip install --upgrade pip
-python3 -m pip install ansible
-
-echo $PATH
-which ansible
-# ansible-galaxy collection install community.general
-# ansible-galaxy collection install ansible.posix
-# sudo mkdir -p /usr/share/ansible/collections
-# sudo cp -r /root/.ansible/collections/ansible_collections /usr/share/ansible/collections/
-# sudo chmod -R a+rx /usr/share/ansible/collections/
 # install ssm agent
 mkdir /tmp/ssm
 cd /tmp/ssm
--- scripts/deb/ubuntu.sh
+++ scripts/deb/ubuntu.sh
@@ -1,35 +1,15 @@
 #!/bin/bash
 # install Ansible and PPA
-# install Ansible and PPA
-# sudo apt update && sudo apt -y upgrade
-# sudo apt -y install software-properties-common
-# sudo apt-add-repository -y ppa:ansible/ansible
-# sudo add-apt-repository -y ppa:deadsnakes/ppa
-# sudo add-apt-repository -y universe
-# # sudo apt install python3.8 python3-virtualenv
-# # sudo echo "ubuntu ALL=(ALL) NOPASSWD:ALL" | sudo tee /etc/sudoers.d/ubuntu
-# sudo apt install ansible -y
-# # sudo apt install python3-pip
-# # pip3 install ansible
-# # virtualenv -p python3.8 venv-ansible
-# # source venv-ansible/bin/activate
-# # pip install ansible
-# # pip freeze
-sudo apt -y update && sudo apt -y upgrade
-sudo apt -y install software-properties-common
-sudo apt-add-repository ppa:ansible/ansible
-sudo apt-get update
-sudo apt -y update
-sudo apt -y install ansible
+sudo apt -y update 
+sudo apt-get upgrade -y
+python3
+sudo apt install python3-pip -y
+sudo apt install python3-virtualenv
+virtualenv -p python3 venv-ansible
+source venv-ansible/bin/activate
+pip list
+pip install ansible
 ansible --version
-echo $PATH
-which ansible
-mkdir -p ~/.ansible/collections/ansible_collections
-ansible-galaxy collection install ansible.posix
-ansible-galaxy collection install community.general
-sudo mkdir -p /usr/share/ansible/collections
-sudo cp -r /root/.ansible/collections/ansible_collections /usr/share/ansible/collections/
-sudo chmod -R a+rx /usr/share/ansible/collections/
 # install ssm agent
 sudo snap install amazon-ssm-agent --classic
 sudo snap start amazon-ssm-agent
