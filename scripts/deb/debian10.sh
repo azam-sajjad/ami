8ee5f175d89e2e3cf551a7d66ec4100c5eff9f4e
azamsajjad <azamsaws@gmail.com>
2024-04-23 22:52:44 +0500

    deb

--- scripts/deb/debian10.sh
+++ scripts/deb/debian10.sh
@@ -5,13 +5,21 @@ sudo apt update
 sudo apt -y install software-properties-common
 sudo apt-add-repository -y ppa:ansible/ansible
 sudo add-apt-repository -y ppa:deadsnakes/ppa
-sudo add-apt-repository -y universe
+sudo apt install build-essential zlib1g-dev libncurses5-dev libgdbm-dev libnss3-dev libssl-dev libreadline-dev libffi-dev libsqlite3-dev wget libbz2-dev
+wget https://www.python.org/ftp/python/3.10.0/Python-3.10.0.tgz
+tar -xvf Python-3.10.0.tgz
+cd Python-3.10.0
+sudo ./configure --enable-optimizations
+make -j $(nproc)
+sudo make altinstall
 sudo apt -y update 
 sudo apt-get upgrade -y
+python3.10 --version
+pip3.10 install --user --upgrade pip
 python3
 sudo apt install python3-pip -y
-sudo apt install virtualenv -y
-virtualenv -p python3 venv-ansible
+sudo apt install python3-virtualenv -y
+python3.10 -m venv venv-ansible
 source venv-ansible/bin/activate
 pip list
 pip install ansible
