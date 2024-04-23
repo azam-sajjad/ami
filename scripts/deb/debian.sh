952e514cf8e61f4b6d4e1a320202c29eada86071
azamsajjad <azamsaws@gmail.com>
2024-04-23 21:49:19 +0500

    deb

--- scripts/deb/debian.sh
+++ scripts/deb/debian.sh
@@ -3,7 +3,7 @@
 echo "127.0.0.1 `hostname` localhost" >> /etc/hosts
 sudo apt -y update 
 sudo apt -y install software-properties-common
-sudo apt-add-repository ppa:ansible/ansible
+sudo apt-add-repository --yes --update ppa:ansible/ansible
 sudo apt-get update --fix-missing -y
 sudo apt -y install ansible
 ansible --version
