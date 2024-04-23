08678fa56685bf415785b1118d87f799811a5eb5
azamsajjad <azamsaws@gmail.com>
2024-04-23 14:33:52 +0500

    debian

--- scripts/deb/ubuntu.sh
+++ scripts/deb/ubuntu.sh
@@ -4,7 +4,8 @@ sudo apt update && sudo apt -y upgrade
 sudo apt -y install software-properties-common
 sudo apt-add-repository ppa:ansible/ansible
 sudo add-apt-repository -y ppa:deadsnakes/ppa
-sudo apt update
+sudo add-apt-repository universe
+sudo apt-get update
 # sudo apt install python3.8 python3-virtualenv
 sudo echo "ubuntu ALL=(ALL) NOPASSWD:ALL" | sudo tee /etc/sudoers.d/ubuntu
 # sudo apt install ansible-base -y
