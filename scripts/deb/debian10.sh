1432668a9d9abfb0941ad30db5d924b0a4e0fcc8
azamsajjad <azamsaws@gmail.com>
2024-04-24 15:04:41 +0500

    deb10

--- scripts/deb/debian10.sh
+++ scripts/deb/debian10.sh
@@ -5,7 +5,7 @@ sudo apt-get update -y
 # sudo apt -y install software-properties-common
 # sudo apt-add-repository -y ppa:ansible/ansible
 # sudo add-apt-repository -y ppa:deadsnakes/ppa
-sudo apt install build-essential zlib1g-dev libncurses5-dev libgdbm-dev libnss3-dev libssl-dev libreadline-dev libffi-dev libsqlite3-dev wget libbz2-dev
+sudo apt install build-essential zlib1g-dev libncurses5-dev libgdbm-dev libnss3-dev libssl-dev libreadline-dev libffi-dev libsqlite3-dev wget libbz2-dev -y
 sudo wget https://www.python.org/ftp/python/3.10.0/Python-3.10.0.tgz
 tar -xvf Python-3.10.0.tgz
 cd Python-3.10.0
