22a5c1cf79b093a151c1d58e1488163257e5d65c
azamsajjad <azamsaws@gmail.com>
2024-04-24 22:02:44 +0500

    deb

--- scripts/deb/debian.sh
+++ scripts/deb/debian.sh
@@ -22,7 +22,7 @@ then
 else
     sudo apt install build-essential zlib1g-dev libncurses5-dev libgdbm-dev libnss3-dev libssl-dev libreadline-dev libffi-dev libsqlite3-dev wget libbz2-dev -y 1> /dev/null
     sudo apt install python3-pip
-    python3 -m pip install ansible
+    pip3 install ansible
     ansible --version
     ansible-community --version
 fi
