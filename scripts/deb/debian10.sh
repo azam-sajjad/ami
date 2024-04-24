8d0cdb9d5dbe1eb4613dee04bed3d59a8ab5ad21
azamsajjad <azamsaws@gmail.com>
2024-04-24 15:00:48 +0500

    deb10

--- scripts/deb/debian10.sh
+++ scripts/deb/debian10.sh
@@ -6,13 +6,12 @@ sudo apt-get update -y
 # sudo apt-add-repository -y ppa:ansible/ansible
 # sudo add-apt-repository -y ppa:deadsnakes/ppa
 sudo apt install build-essential zlib1g-dev libncurses5-dev libgdbm-dev libnss3-dev libssl-dev libreadline-dev libffi-dev libsqlite3-dev wget libbz2-dev
-wget https://www.python.org/ftp/python/3.10.0/Python-3.10.0.tgz
+sudo wget https://www.python.org/ftp/python/3.10.0/Python-3.10.0.tgz
 tar -xvf Python-3.10.0.tgz
 cd Python-3.10.0
 sudo ./configure --enable-optimizations
-make -j $(nproc)
+sudo make -j $(nproc)
 sudo make altinstall
-python3.10 --version
 /usr/local/bin/python3.10 -m pip install --upgrade pip
 sudo apt install python3-pip -y
 sudo apt install python3-virtualenv -y
