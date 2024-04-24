c0dd31d0a811ace1277f1d137c56b904184cf140
azamsajjad <azamsaws@gmail.com>
2024-04-24 14:50:10 +0500

    deb10

--- scripts/deb/debian10.sh
+++ scripts/deb/debian10.sh
@@ -1,28 +1,24 @@
 #!/bin/bash
 # install Ansible and PPA
-echo "127.0.0.1 `hostname` localhost" >> /etc/hosts
-sudo apt update
-sudo apt -y install software-properties-common
-sudo apt-add-repository -y ppa:ansible/ansible
-sudo add-apt-repository -y ppa:deadsnakes/ppa
+# echo "127.0.0.1 `hostname` localhost" >> /etc/hosts
+sudo apt-get update
+# sudo apt -y install software-properties-common
+# sudo apt-add-repository -y ppa:ansible/ansible
+# sudo add-apt-repository -y ppa:deadsnakes/ppa
 sudo apt install build-essential zlib1g-dev libncurses5-dev libgdbm-dev libnss3-dev libssl-dev libreadline-dev libffi-dev libsqlite3-dev wget libbz2-dev
 wget https://www.python.org/ftp/python/3.10.0/Python-3.10.0.tgz
 tar -xvf Python-3.10.0.tgz
 cd Python-3.10.0
 sudo ./configure --enable-optimizations
-make -j $(nproc)
-sudo make altinstall
-sudo apt -y update 
-sudo apt-get upgrade -y
+make -j $(nproc) 1> /dev/null
+sudo make altinstall 1> /dev/null
 python3.10 --version
-pip3.10 install --user --upgrade pip
-python3
+/usr/local/bin/python3.10 -m pip install --upgrade pip
 sudo apt install python3-pip -y
 sudo apt install python3-virtualenv -y
-virtualenv -p python3 venv-ansible
-# python3.10 -m venv venv-ansible
+python3.10 -m venv venv-ansible
 source venv-ansible/bin/activate
-pip list
+/root/venv-ansible/bin/python3.10 -m pip install --upgrade pip
 pip install ansible
 ansible --version
 ansible-community --version
