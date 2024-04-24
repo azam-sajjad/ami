c4d71bb0517f5045292a6b43d0df60d77df42a6c
azamsajjad <azamsaws@gmail.com>
2024-04-24 17:07:51 +0500

    pipeline

--- scripts/deb/debian.sh
+++ scripts/deb/debian.sh
@@ -1,26 +1,23 @@
 #!/bin/bash
-# install Ansible and PPA
-echo "127.0.0.1 `hostname` localhost" >> /etc/hosts
-sudo apt update
-sudo apt -y install software-properties-common
-sudo apt-add-repository -y ppa:ansible/ansible
-sudo add-apt-repository -y ppa:deadsnakes/ppa
-sudo add-apt-repository -y universe
-sudo apt -y update 
-sudo apt-get upgrade -y
-python3
-sudo apt install python3-pip -y
-sudo apt install python3-virtualenv
-virtualenv -p python3 venv-ansible
-source venv-ansible/bin/activate
-pip list
-pip install ansible
+# install Ansible with Python3.10 Virtual Environment
+sudo apt-get update -y 1> /dev/null
+sudo apt install build-essential zlib1g-dev libncurses5-dev libgdbm-dev libnss3-dev libssl-dev libreadline-dev libffi-dev libsqlite3-dev wget libbz2-dev -y 1> /dev/null
+sudo wget https://www.python.org/ftp/python/3.10.0/Python-3.10.0.tgz 1> /dev/null
+tar -xvf Python-3.10.0.tgz 1> /dev/null
+echo "Python3.10 installation will take 5+ minutes!"
+cd Python-3.10.0
+sudo ./configure --enable-optimizations 1> /dev/null
+sudo make -j $(nproc) 1> /dev/null
+sudo make altinstall 1> /dev/null
+cd ..
+/usr/local/bin/python3.10 -m pip install --upgrade pip
+/usr/local/bin/python3.10 -m pip install ansible
 ansible --version
 ansible-community --version
 # install ssm agent
 mkdir /tmp/ssm
 cd /tmp/ssm
-wget https://s3.amazonaws.com/ec2-downloads-windows/SSMAgent/latest/debian_amd64/amazon-ssm-agent.deb
+wget https://s3.amazonaws.com/ec2-downloads-windows/SSMAgent/latest/debian_amd64/amazon-ssm-agent.deb 1> /dev/null
 sudo dpkg -i amazon-ssm-agent.deb
 sudo systemctl enable amazon-ssm-agent
 sudo systemctl start amazon-ssm-agent
