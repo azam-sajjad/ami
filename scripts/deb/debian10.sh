9a36972123de9a2b477aea6cd25fdb2a14b1e2e5
azamsajjad <azamsaws@gmail.com>
2024-04-24 16:46:44 +0500

    pipeline

--- scripts/deb/debian10.sh
+++ scripts/deb/debian10.sh
@@ -4,11 +4,7 @@ sudo apt-get update -y 1> /dev/null
 sudo apt install build-essential zlib1g-dev libncurses5-dev libgdbm-dev libnss3-dev libssl-dev libreadline-dev libffi-dev libsqlite3-dev wget libbz2-dev -y 1> /dev/null
 sudo wget https://www.python.org/ftp/python/3.10.0/Python-3.10.0.tgz 1> /dev/null
 tar -xvf Python-3.10.0.tgz 1> /dev/null
-########################################################
-########################################################
 echo "Python3.10 installation will take 5+ minutes!"
-########################################################
-########################################################
 cd Python-3.10.0
 sudo ./configure --enable-optimizations 1> /dev/null
 sudo make -j $(nproc) 1> /dev/null
@@ -18,11 +14,11 @@ cd ..
 sudo apt install python3-pip -y 1> /dev/null
 sudo apt install python3-virtualenv -y 1> /dev/null
 sudo cd /tmp
-python3.10 -m venv venv-ansible
+sudo python3.10 -m venv /tmp/venv-ansible
 ls -alh /home/admin
-ls -alh .
-source venv-ansible/bin/activate
-pip install ansible
+sudo ls -alh .
+sudo source /tmp/venv-ansible/bin/activate
+sudo pip install ansible
 ansible --version
 ansible-community --version
 # install ssm agent
