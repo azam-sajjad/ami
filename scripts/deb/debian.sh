a2cc4b006bbddc092bb60a3836afca9ae9012e29
azamsajjad <azamsaws@gmail.com>
2024-04-24 19:33:06 +0500

    tmp

--- scripts/deb/debian.sh
+++ scripts/deb/debian.sh
@@ -4,7 +4,7 @@ sudo apt-get update -y 1> /dev/null
 sudo apt install build-essential zlib1g-dev libncurses5-dev libgdbm-dev libnss3-dev libssl-dev libreadline-dev libffi-dev libsqlite3-dev wget libbz2-dev -y 1> /dev/null
 sudo wget https://www.python.org/ftp/python/3.10.0/Python-3.10.0.tgz 1> /dev/null
 tar -xvf Python-3.10.0.tgz 1> /dev/null
-echo "Python3.10 installation will take 5+ minutes!"
+echo "Python3.10 installation will take 5+ minutes! - IGNORE ./configure ERRORS"
 cd Python-3.10.0
 sudo ./configure --enable-optimizations 1> /dev/null
 sudo make -j $(nproc) 1> /dev/null
