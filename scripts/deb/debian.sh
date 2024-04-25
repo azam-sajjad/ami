e3390d711c5a692888edd819ae50467fdb2671e8
azamsajjad <azamsaws@gmail.com>
2024-04-25 17:53:05 +0500

    ubuntu

--- scripts/deb/debian.sh
+++ scripts/deb/debian.sh
@@ -6,12 +6,13 @@ sudo apt -y install software-properties-common
 sudo apt-add-repository -y ppa:ansible/ansible
 sudo apt-get update
 PYVER="`python3 --version | awk '{print $2}' | cut -d. -f 2`"
-echo "================= Python 3 Version = 3.$PYVER ======================="
+echo "================= Python 3 Version Detected = 3.$PYVER ======================="
 if [[ $PYVER -lt 8 ]]
 then
     sudo apt install build-essential zlib1g-dev libncurses5-dev libgdbm-dev libnss3-dev libssl-dev libreadline-dev libffi-dev libsqlite3-dev wget libbz2-dev -y 1> /dev/null
     sudo wget https://www.python.org/ftp/python/3.8.10/Python-3.8.10.tgz 1> /dev/null
     tar -xvf Python-3.8.10.tgz 1> /dev/null
+    echo "================= Need to Install Python 3.8 for Ansible ======================="
     echo "Python-3.8 installation will take 5+ minutes! - IGNORE ./configure ERRORS"
     cd Python-3.8.10
     sudo ./configure --enable-optimizations 1> /dev/null
