3babb0e4e9184943f0b5cf56369b6f28940de030
azamsajjad <azamsaws@gmail.com>
2024-04-25 18:21:20 +0500

    ubuntu

--- scripts/deb/debian.sh
+++ scripts/deb/debian.sh
@@ -9,18 +9,18 @@ PYVER="`python3 --version | awk '{print $2}' | cut -d. -f 2`"
 echo "================= Python 3 Version Detected = 3.$PYVER ======================="
 if [[ $PYVER -lt 8 ]]
 then
-    sudo apt install build-essential zlib1g-dev libncurses5-dev libgdbm-dev libnss3-dev libssl-dev libreadline-dev libffi-dev libsqlite3-dev wget libbz2-dev -y 1> /dev/null
-    sudo wget https://www.python.org/ftp/python/3.8.10/Python-3.8.10.tgz 1> /dev/null
-    tar -xvf Python-3.8.10.tgz 1> /dev/null
-    echo "================= Need to Install Python 3.8 for Ansible ======================="
-    echo "Python-3.8 installation will take 5+ minutes! - IGNORE ./configure ERRORS"
-    cd Python-3.8.10
+    sudo apt-get install build-essential zlib1g-dev libncurses5-dev libncursesw5-dev libgdbm-dev libnss3-dev libssl-dev libreadline-dev libffi-dev libsqlite3-dev wget curl libbz2-dev xz-utils tk-dev liblzma-dev python3-openssl -y 1> /dev/null
+    sudo wget https://www.python.org/ftp/python/3.10.4/Python-3.10.4.tgz 1> /dev/null
+    tar -xvf Python-3.10.4.tgz 1> /dev/null
+    echo "================= Need to Install Python 3.10 for Ansible ======================="
+    echo "Python-3.10 installation will take 5+ minutes! - IGNORE ./configure ERRORS"
+    cd Python-3.10.4
     sudo ./configure --enable-optimizations 1> /dev/null
     sudo make -j $(nproc) 1> /dev/null
     sudo make altinstall 1> /dev/null
     cd ..
-    /usr/bin/python3.8 -m pip install --upgrade pip
-    /usr/bin/python3.8 -m pip install ansible
+    /usr/bin/python3.10 -m pip install --upgrade pip
+    /usr/bin/python3.10 -m pip install ansible
     ansible --version
     ansible-community --version
 else
