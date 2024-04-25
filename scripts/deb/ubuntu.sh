6fcc3a6c6f4cbc930363829c86da326d8094e2ba
azamsajjad <azamsaws@gmail.com>
2024-04-25 16:51:17 +0500

    f

--- scripts/deb/ubuntu.sh
+++ scripts/deb/ubuntu.sh
@@ -6,13 +6,13 @@ sudo apt-add-repository -y ppa:ansible/ansible
 sudo apt-get update
 PYVER="`python3 --version | awk '{print $2}' | cut -d. -f 2`"
 echo "PYVER=$PYVER"
-if [[ $PYVER -lt 10 ]]
+if [[ $PYVER -lt 8 ]]
 then
     sudo apt install build-essential zlib1g-dev libncurses5-dev libgdbm-dev libnss3-dev libssl-dev libreadline-dev libffi-dev libsqlite3-dev wget libbz2-dev -y 1> /dev/null
-    sudo wget https://www.python.org/ftp/python/3.10.4/Python-3.10.4.tgz 1> /dev/null
-    tar -xvf Python-3.10.4.tgz 1> /dev/null
+    sudo wget https://www.python.org/ftp/python/3.8.10/Python-3.8.10.tgz 1> /dev/null
+    tar -xvf Python-3.8.10.tgz 1> /dev/null
     echo "Python3.10 installation will take 5+ minutes! - IGNORE ./configure ERRORS"
-    cd Python-3.10.4
+    cd Python-3.8.10
     sudo ./configure --enable-optimizations 1> /dev/null
     sudo make -j $(nproc) 1> /dev/null
     sudo make altinstall 1> /dev/null
