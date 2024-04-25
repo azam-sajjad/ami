3ee7b550ad183eb78b702d822b0abdf216d10c07
azamsajjad <azamsaws@gmail.com>
2024-04-25 19:31:49 +0500

    deb

--- scripts/deb/debian.sh
+++ scripts/deb/debian.sh
@@ -19,6 +19,8 @@ then
     sudo make -j $(nproc) 1> /dev/null
     sudo make altinstall 1> /dev/null
     cd ..
+    which python3.10
+    sudo ls -alh /usr/bin
     /usr/bin/python3.10 -m pip install --upgrade pip
     /usr/bin/python3.10 -m pip install ansible
     ansible --version
