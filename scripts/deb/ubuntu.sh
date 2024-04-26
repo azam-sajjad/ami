b95d554242ddacc9a09b134624fe17902497d829
azamsajjad <azamsaws@gmail.com>
2024-04-26 16:09:44 +0500

    u18

--- scripts/deb/ubuntu.sh
+++ scripts/deb/ubuntu.sh
@@ -4,28 +4,32 @@ sudo apt-get update -y
 sudo apt -y install software-properties-common
 sudo apt-add-repository -y ppa:ansible/ansible
 sudo add-apt-repository -y ppa:deadsnakes/ppa
+sudo apt update
 sudo apt-get update
 PYVER="`python3 --version | awk '{print $2}' | cut -d. -f 2`"
 echo "================= Python 3 Version Detected = 3.$PYVER ======================="
 if [[ $PYVER -lt 8 ]]
 then
     # sudo echo "deb http://ppa.launchpad.net/ansible/ansible/ubuntu trusty main" >> /etc/apt/sources.list
-    sudo apt-get install build-essential zlib1g-dev libncurses5-dev libncursesw5-dev libgdbm-dev libnss3-dev libssl-dev libreadline-dev libffi-dev libsqlite3-dev wget curl libbz2-dev xz-utils tk-dev liblzma-dev python3-openssl -y 1> /dev/null
-    sudo wget https://www.python.org/ftp/python/3.10.4/Python-3.10.4.tgz 1> /dev/null
-    tar -xvf Python-3.10.4.tgz 1> /dev/null
-    echo "================= Need to Install Python 3.10 for Ansible ======================="
-    echo "Python-3.10 installation will take 5+ minutes! - IGNORE ./configure ERRORS"
-    cd Python-3.10.4
-    sudo ./configure --enable-optimizations 1> /dev/null
-    sudo make -j $(nproc) 1> /dev/null
-    sudo make altinstall 1> /dev/null
-    cd ..
-    which python3.10
-    sudo apt install python3-pip python3-setuptools python3-wheel --yes --quiet
-    /usr/local/bin/python3.10 -m pip install --upgrade pip
-    /usr/local/bin/python3.10 -m pip install ansible 
-    sudo pip3 install ansible
-    sudo chown ubuntu:ubuntu -R /home/ubuntu
+    # sudo apt-get install build-essential zlib1g-dev libncurses5-dev libncursesw5-dev libgdbm-dev libnss3-dev libssl-dev libreadline-dev libffi-dev libsqlite3-dev wget curl libbz2-dev xz-utils tk-dev liblzma-dev python3-openssl -y 1> /dev/null
+    # sudo wget https://www.python.org/ftp/python/3.10.4/Python-3.10.4.tgz 1> /dev/null
+    # tar -xvf Python-3.10.4.tgz 1> /dev/null
+    # echo "================= Need to Install Python 3.10 for Ansible ======================="
+    # echo "Python-3.10 installation will take 5+ minutes! - IGNORE ./configure ERRORS"
+    # cd Python-3.10.4
+    # sudo ./configure --enable-optimizations 1> /dev/null
+    # sudo make -j $(nproc) 1> /dev/null
+    # sudo make altinstall 1> /dev/null
+    # cd ..
+    # which python3.10
+    # sudo apt install python3-pip python3-setuptools python3-wheel --yes --quiet
+    # /usr/local/bin/python3.10 -m pip install --upgrade pip
+    # /usr/local/bin/python3.10 -m pip install ansible 
+    # sudo pip3 install ansible
+    # sudo chown ubuntu:ubuntu -R /home/ubuntu
+    sudo apt install ansible
+    sudo apt install python-pip
+    pip install boto3 --user 
     ansible --version
     ansible-community --version
 else
