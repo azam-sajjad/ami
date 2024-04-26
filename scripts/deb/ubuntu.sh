9cf7643742aec23fc6968b723217eb2eea3a9382
azamsajjad <azamsaws@gmail.com>
2024-04-26 17:46:33 +0500

    u18

--- scripts/deb/ubuntu.sh
+++ scripts/deb/ubuntu.sh
@@ -23,6 +23,9 @@ then
     sudo apt install python3-pip python3-setuptools python3-wheel --yes --quiet
     /usr/local/bin/python3.10 -m pip install --upgrade pip
     /usr/local/bin/python3.10 -m pip install ansible  
+    which ansible
+    echo $PATH
+    sudo echo $PATH
     ansible --version
     ansible-community --version
 else
