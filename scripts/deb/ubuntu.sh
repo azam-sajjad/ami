0c1eeb53884d74468d9cb0eaf074a317695b7f58
azamsajjad <azamsaws@gmail.com>
2024-04-26 20:08:21 +0500

    u18

--- scripts/deb/ubuntu.sh
+++ scripts/deb/ubuntu.sh
@@ -10,21 +10,27 @@ PYVER="`python3 --version | awk '{print $2}' | cut -d. -f 2`"
 echo "================= Python 3 Version Detected = 3.$PYVER ======================="
 if [[ $PYVER -lt 8 ]]
 then
+    sudo apt update
+    sudo apt install python3.8 -y
+    sudo apt install python3.8-dev python3.8-venv -y
+    sudo update-alternatives --install /usr/bin/python3 python3 /usr/bin/python3.$PYVER 1
+    sudo update-alternatives --install /usr/bin/python3 python3 /usr/bin/python3.8 2
+    sudo update-alternatives --config python3 | 2
     sudo apt-get install build-essential zlib1g-dev libncurses5-dev libncursesw5-dev libgdbm-dev libnss3-dev libssl-dev libreadline-dev libffi-dev libsqlite3-dev wget curl libbz2-dev xz-utils tk-dev liblzma-dev python3-openssl -y 1> /dev/null
-    sudo wget https://www.python.org/ftp/python/3.10.4/Python-3.10.4.tgz 1> /dev/null
-    tar -xvf Python-3.10.4.tgz 1> /dev/null
-    echo "================= Setting up Python 3.10 for Ansible ======================="
-    echo "Python-3.10 installation will take 5+ minutes! - IGNORE ./configure ERRORS"
-    cd Python-3.10.4
-    sudo ./configure --enable-optimizations 1> /dev/null
-    sudo make -j $(nproc) 1> /dev/null
-    sudo make altinstall 1> /dev/null
-    cd ..
+    # sudo wget https://www.python.org/ftp/python/3.10.4/Python-3.10.4.tgz 1> /dev/null
+    # tar -xvf Python-3.10.4.tgz 1> /dev/null
+    # echo "================= Setting up Python 3.10 for Ansible ======================="
+    # echo "Python-3.10 installation will take 5+ minutes! - IGNORE ./configure ERRORS"
+    # cd Python-3.10.4
+    # sudo ./configure --enable-optimizations 1> /dev/null
+    # sudo make -j $(nproc) 1> /dev/null
+    # sudo make altinstall 1> /dev/null
+    # cd ..
     sudo apt install python3-pip python3-setuptools python3-wheel --yes --quiet
-    /usr/local/bin/python3.10 -m pip install --upgrade pip
-    /usr/local/bin/python3.10 -m pip install ansible --user ansible 
-    export PATH="$PATH:$HOME/.local/bin:$HOME/bin"
-    which ansible-playbook
+    # /usr/local/bin/python3.10 -m pip install --upgrade pip
+    python3 -m pip install ansible --user ansible 
+    # export PATH="$PATH:$HOME/.local/bin:$HOME/bin"
+    # which ansible-playbook
     echo $PATH
     sudo echo $PATH
     ansible --version
