6c3441f05c575377957674229f7f5fc3a369f58d
azamsajjad <azamsaws@gmail.com>
2024-04-26 21:40:52 +0500

    u18

--- scripts/deb/ubuntu.sh
+++ scripts/deb/ubuntu.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# install Ansible with Python3.10
+# install Ansible
 sudo apt-get update -y 
 sudo apt -y install software-properties-common
 sudo apt-add-repository -y ppa:ansible/ansible
@@ -10,11 +10,6 @@ PYVER="`python3 --version | awk '{print $2}' | cut -d. -f 2`"
 echo "================= Python 3 Version Detected = 3.$PYVER ======================="
 if [[ $PYVER -lt 8 ]]
 then
-    # echo "================= Setting up Python 3.8 for Ansible ======================="
-    # pip --version
-    # sudo apt install python3.8 -y
-    # sudo apt install python3.8-dev python3.8-venv -y
-    # sudo apt install python3-pip python3-setuptools python3-wheel --yes --quiet
     sudo apt install ansible -y
     ansible-galaxy collection install ansible.posix
     ansible-galaxy collection install community.general
